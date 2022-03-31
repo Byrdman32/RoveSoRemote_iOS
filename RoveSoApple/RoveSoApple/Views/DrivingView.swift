c77efd098cb3078e076b4f88c1e6712f0d2894d9
Byrdman32 <eli@thebyrdnest.net>
2022-03-30 20:26:38 -0500

    Driving UI Complete

--- RoveSoApple/RoveSoApple/Views/DrivingView.swift
+++ RoveSoApple/RoveSoApple/Views/DrivingView.swift
@@ -6,11 +6,31 @@
 //
 
 import SwiftUI
+import OMJoystick
+import SFSafeSymbols
 
 struct DrivingView: View {
+    
+    let iconSetting = IconSetting(
+        leftIcon: Image(systemSymbol: .arrowLeft),
+        rightIcon: Image(systemSymbol: .arrowRight),
+        upIcon: Image(systemSymbol: .arrowUp),
+        downIcon: Image(systemSymbol: .arrowDown)
+    )
+    
+    let colorSetting = ColorSetting(subRingColor: .red, bigRingNormalBackgroundColor: .green, bigRingDarkBackgroundColor: .blue, bigRingStrokeColor: .yellow)
+    
     var body: some View {
         NavigationView {
             ZStack {
+                HStack {
+                    OMJoystick(isDebug: false, iconSetting: self.iconSetting, colorSetting: ColorSetting(), smallRingRadius: 30, bigRingRadius: 70) {
+                        (JoyStickState, stickPosition) in
+                    }
+                    OMJoystick(isDebug: false, iconSetting: self.iconSetting, colorSetting: ColorSetting(), smallRingRadius: 30, bigRingRadius: 70) {
+                        (JoyStickState, stickPosition) in
+                    }
+                }
             }
             .navigationTitle("Driving")
         }
