4e95d00320294e121a288e2369478a02837d0ac7
Byrdman32 <eli@thebyrdnest.net>
2022-03-30 21:10:35 -0500

    Autonomy UI Complete

--- RoveSoApple/RoveSoApple/Views/AutonomyView.swift
+++ RoveSoApple/RoveSoApple/Views/AutonomyView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct AutonomyView: View {
+    
+    @State private var latitude: Float = 34.746481
+    @State private var longitude: Float = -92.289595
+    
     var body: some View {
         NavigationView {
-            ZStack {
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("Latitude: \(latitude)")
+                    Spacer()
+                    Text("Longitude: \(longitude)")
+                    Spacer()
+                }
+                VStack {
+                    Button("Send Current Position as Waypoint") {
+                        print("\(latitude), \(longitude)")
+                    }
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(15)
+                    
+                    Button("Clear Waypoints") {
+                        print("Waypoints Cleared")
+                    }
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(15)
+                }
+                Spacer()
+                VStack {
+                    Button("Start Autonomy") {
+                        print("Starting Autonomy")
+                    }
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(15)
+                    
+                    Button("Disable Autonomy") {
+                        print("Ending Autonomy")
+                    }
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(15)
+                }
+                Spacer()
             }
             .navigationTitle("Autonomy")
         }
